adb0269f1634b53f7b65fb73961954c9f86cc88d
Aziz <azizbahssou444@gmail.com>
2025-07-22 20:05:15 +0100

    edit loop file

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,7 @@
-const lines = ['Cis fun', 'Python is cool', 'Javascript is amazing',];
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
 
-for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
-}
+for (const line of lines) console.log(line);
